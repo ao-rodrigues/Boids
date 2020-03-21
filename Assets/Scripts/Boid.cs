43a763adf8dce6e536c5fe79f05689ff98cb0d73
André Rodrigues <androd97@gmail.com>
2020-03-21 16:27:59 +0000

    Added a "General DIrection" field

--- Assets/Scripts/Boid.cs
+++ Assets/Scripts/Boid.cs
@@ -4,7 +4,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Boid : MonoBehaviour {
-    public Vector3 velocity = new Vector3(1, 0, 0);
+    [HideInInspector]
+    public Vector3 velocity = Vector3.zero;
+    
+    public Vector3 generalDirection = new Vector3(1, 0, 0);
     public float maxVelocity = 5;
     public float awarenessRadius = 5;
     
@@ -14,11 +17,11 @@ public class Boid : MonoBehaviour {
             velocity = velocity.normalized * maxVelocity;
         }
         
-        transform.position += velocity * Time.deltaTime;
-        transform.rotation = Quaternion.LookRotation(velocity);
+        transform.position += (generalDirection + velocity) * Time.deltaTime;
+        transform.rotation = Quaternion.LookRotation(generalDirection + velocity);
     }
 
-    private void OnDrawGizmos(){
+    private void OnDrawGizmosSelected(){
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, awarenessRadius);
     }
