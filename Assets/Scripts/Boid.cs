c51d65e99085de1364383c469f46f4bdec4c87fe
André Rodrigues <androd97@gmail.com>
2020-03-21 16:06:10 +0000

    Implement Boid base script

--- Assets/Scripts/Boid.cs
+++ Assets/Scripts/Boid.cs
@@ -1,13 +1,25 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Boid : MonoBehaviour {
-    public Vector3 velocity;
-
+    public Vector3 velocity = new Vector3(1, 0, 0);
+    public float maxVelocity = 5;
+    public float awarenessRadius = 5;
+    
     // Update is called once per frame
     void Update(){
+        if (velocity.magnitude > maxVelocity) {
+            velocity = velocity.normalized * maxVelocity;
+        }
+        
         transform.position += velocity * Time.deltaTime;
         transform.rotation = Quaternion.LookRotation(velocity);
+    }
+
+    private void OnDrawGizmos(){
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, awarenessRadius);
     }
 }
