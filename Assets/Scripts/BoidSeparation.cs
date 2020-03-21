f70c169f63e0a4979789b005024da447ee2ecd54
André Rodrigues <androd97@gmail.com>
2020-03-21 16:07:03 +0000

    Implement Boid Separation behaviour

--- Assets/Scripts/BoidSeparation.cs
+++ Assets/Scripts/BoidSeparation.cs
@@ -1,18 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
-public class BoidSeparation : MonoBehaviour
-{
+[RequireComponent(typeof(Boid))]
+public class BoidSeparation : MonoBehaviour {
+    public float minDistance = 0.25f;
+
+    private Boid boid;
+    private Boid[] boids;
+    
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start(){
+        boid = GetComponent<Boid>();
+        boids = FindObjectsOfType<Boid>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update(){
+        Vector3 displacement = Vector3.zero;
         
+        foreach (var b in boids.Where(b => b != this.boid)) {
+            var dist = b.transform.position - boid.transform.position;
+
+            if (dist.magnitude < minDistance) {
+                displacement -= dist;
+            }
+        }
+
+        boid.velocity += displacement;
     }
 }
