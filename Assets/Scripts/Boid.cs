34c8398403ff7c90127ef4bd9d984d48455a0fd1
André Rodrigues <androd97@gmail.com>
2020-03-21 00:11:56 +0000

    Add project files

---
+++ Assets/Scripts/Boid.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boid : MonoBehaviour {
+    public Vector3 velocity;
+
+    // Update is called once per frame
+    void Update(){
+        transform.position += velocity * Time.deltaTime;
+        transform.rotation = Quaternion.LookRotation(velocity);
+    }
+}
---
+++ Assets/Scripts/BoidCohesion.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Boid))]
+public class BoidCohesion : MonoBehaviour {
+
+    private Boid boid;
+    
+    // Start is called before the first frame update
+    void Start(){
+        boid = GetComponent<Boid>();
+    }
+
+    // Update is called once per frame
+    void Update(){
+    }
+}
